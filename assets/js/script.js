24a4a0e92e2ed93d3eac824e167750b50ded5198
Heather Everton <heather.everton@planplusonline.com>
2020-08-19 21:14:40 -0600

    added edit and delete buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
+
 //this can be document instead of window.document. 
 //This line of code is searching the HTML file for a button element.
 window.document.querySelector("button");
@@ -40,6 +43,9 @@ var createTaskEl = function(taskDataObj) {
     //this appliies the CSS to the element that gets added to the DOM
     listItemEl.className = "task-item";
     
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     // give it a class name
@@ -47,76 +53,116 @@ var createTaskEl = function(taskDataObj) {
     // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
-    listItemEl.appendChild(taskInfoEl);
-
     //returns the first element within the html file that matches the id task-to-do (line 20)
     var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(taskIdCounter);
+    listItemEl.appendChild(taskActionsEl);
+
+    tasksToDoEl.appendChild(listItemEl);;
+
+
+
     //appendChild means to add a new element to the dom and this will display on the webpage. 
     tasksToDoEl.appendChild(listItemEl);
+
+    // increase task counter for next unique id
+    taskIdCounter++;
 }
+
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+    
+    // create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    
+
+    actionContainerEl.appendChild(editButtonEl);
+    
+    // create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+    
+    // create status drop down
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(statusSelectEl);
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i = 0; i < statusChoices.length; i++) {
+        // create option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+      
+        // append to select
+        statusSelectEl.appendChild(statusOptionEl);
+      }
+    
+    return actionContainerEl;
+};
 
 
 //this is looking at the formEL variable we set above (line 7 of JS file) and adding an action to "listen" for. 
 formEl.addEventListener("submit", taskFormHandler);
 
-
-
-
-
-
-
-
-
-
-
-
-// //this can be document instead of window.document. 
-// //This line of code is searching the HTML file for a button element.
-// window.document.querySelector("button");
-
-// //this assigns a var to an object from the document. 
-// //This time we're using the ID to search for the object in the HTML document
-// var formEl = document.querySelector("#task-form");
-
-// var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-
-// //this displays the html liine for the element with the #save-task id
-// console.log(formEl);
-
-// var createTaskHandler = function (){
-//     event.preventDefault();
-//     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-//     // create list item
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "task-item";
-
-//     var taskNameInput = document.querySelector("input[name='task-name']");
-//     console.log(taskNameInput);
-
-
+var taskButtonHandler = function(event) {
+    // get target element from event
+    var targetEl = event.target;
     
-
-//     // create div to hold task info and add to list item
-//     var taskInfoEl = document.createElement("div");
-//     // give it a class name
-//     taskInfoEl.className = "task-info";
-//     // add HTML content to div
-//     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-
-//     listItemEl.appendChild(taskInfoEl);
-
-//     // add entire list item to list
-//     tasksToDoEl.appendChild(listItemEl);
-
-//     console.log(event);
-// } 
-
-// //this is looking at the formEl variable we set above (line 7 of JS file) and adding an action to "listen" for 
-// //formEL = variable addEventListener ("submit")= the event its watching for, createTaskHandler = function
-// formEl.addEventListener("submit", createTaskHandler)
+    // edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    } 
+    
+    // delete button was clicked
+    else if (targetEl.matches(".delete-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
 
 
+pageContentEl.addEventListener("click", taskButtonHandler);
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};  
+  
+var editTask = function(taskId) {
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name 
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    //pull the task name into the name form field
+    document.querySelector("input[name='task-name']").value = taskName;
+
+    // get content from task type 
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    //pull the task type into the select form field
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    // change button name from add task to save task
+    document.querySelector("#save-task").textContent = "Save Task";
+
+    formEl.setAttribute("data-task-id", taskId);
 
 
+};
+  
